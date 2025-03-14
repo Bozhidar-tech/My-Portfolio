f8b0567ceb80d0bddfb6eee219f495d498381693
Bozhidar Nunev <124468291+Bozhidar-tech@users.noreply.github.com>
2025-03-15 00:10:13 +0100

    minor changes on navbar

--- client/components/DefaultPage/DefaultPage.jsx
+++ client/components/DefaultPage/DefaultPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./DefaultPage.css";
+import Navbar from "../Navbar/Navbar";
 
 const DefaultPage = () => {
   return (
     <section id="defaultPage" className="defaultPage-section">
+      <Navbar />
       <div className="defaultPage-content">
         <h1>Welcome to my Portfolio</h1>
         <p>I hope you like it!</p>
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import Navbar from "../components/Navbar/Navbar";
 import DefaultPage from "../components/DefaultPage/DefaultPage";
 import Separator from "../components/Separator/Separator";
 import About from "../components/About/About";
@@ -8,7 +7,6 @@ import About from "../components/About/About";
 export default function App() {
   return (
     <div className="App">
-      <Navbar />
       <DefaultPage />
       <Separator />
       <About />
